d0604f93b8db82b1673069f271e27736370090a8
ryansutc <ryansutc@gmail.com>
2016-02-03 18:45:06 -0400

    Major Update & Fix 1
    Major Update / Refactor:
    - Refactored 2D Seats Array to a 1D Array
    - Fixed code for seat placement
    - Fixed code for checkAvailability method

--- Seat.java
+++ Seat.java
@@ -6,12 +6,14 @@ public class Seat {
 	public boolean avail = true; //made public for simplicity/speed
 	public String name = null;
 	private String occupant = null;
-	private char section; //a = first class, b = economy
+	private int section; //1 = first class, 2 = economy
+	private String type; //Window or Aisle
 	
-	public Seat(String name, char section, char type)
+	public Seat(String name, int section, String type)
 	{
 		this.name = name;
 		this.setSection(section);
+		this.setType(type);
 	}
 	
 	public String getOccupant() 
@@ -25,11 +27,19 @@ public class Seat {
 		this.avail = false;
 	}
 
-	public char getSection() {
+	public int getSection() {
 		return section;
 	}
 
-	public void setSection(char section) {
+	public void setSection(int section) {
 		this.section = section;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 }
