543c001feaa2fcdacadd4a333c88152361fea843
ryansutc <ryansutc@gmail.com>
2016-02-03 18:45:47 -0400

    Major Update & Fix 1
    Major Update / Refactor:
    - Refactored 2D Seats Array to a 1D Array
    - Fixed code for seat placement
    - Fixed code for checkAvailability method

--- Seats.java
+++ Seats.java
@@ -8,103 +8,138 @@ package airplaneseatbooker;
 
 public class Seats {
 	
-	public Seat[][] seats; 
-	private int[] availabilityArray = new int[4]; //(ttl, window, aisle, first class, economy)
+	public Seat[] seats; 
+	//private int[] availabilityArray = new int[4]; //(ttl, window, aisle, first class, economy)
 	//constructor
 	public Seats()
 	{
 
-		seats = new Seat[4][4];
+		seats = new Seat[16];
 		
-		for (int col=0; col < 4; col++)
+		for (int i=0; i < 16; i++)
 		{
-			for (int row=0; row < 4; row++)
+			
+			String myseat = "" + getSeat(i);
+			//1 = FirstClass, 2 = Economy
+			int mysection = ((i / 8) > 0) ? 2: 1;
+			String mytype;
+			if (myseat.charAt(1) == 'a' || myseat.charAt(1) == 'd')
 			{
-				String myseat = "" + row + getLetter(col);
-				char mysection = getSection(row);
-				char mytype;
-				if (col == 0 || col == 3)
-				{
-					mytype = 'W';
-				}
-				else 
-				{
-					mytype = 'A';
-				}
-
-				seats[col][row] = new Seat(myseat, mysection, mytype);
+				mytype = "Window";
 			}
+			else 
+			{
+				mytype = "Aisle";
+			}
+
+			seats[i] = new Seat(myseat, mysection, mytype);
+			
 		}
+		
+		/*
+		int x = 0;
+		int y = 0;
+		while ( x < 16)
+		{
+			if (x == 8)
+			{
+				System.out.println("------   -----");
+			}
+			System.out.print("" +(y +1)+ " ");
+			
+			for (int col = 0; col < 4; col++)
+			{
+
+				if (col == 2)
+				{
+					System.out.print("|| ");
+				}
+				
+				if (seats[x].avail == true)
+				{
+					System.out.print("o:" + seats[x].name + seats[x].getSection());
+				}
+				else
+				{
+					System.out.print("x " + seats[x].name + seats[x].getSection());
+				}
+				x +=1;
+			}
+			y +=1;
+			System.out.println("");
+		}
+		*/
 	}
-	
-	//choose a free seat based on criteria and reserve it
-	public void bookSeat(String name, char section, char type){
+
+		
+	//choose a free seat based on criteria and reserve it (owner name, section request, aisle/window)
+	public String bookSeat(String name, int section, String type){
 		
 		//TODO: write code
-		int myclass = (section == '1') ? 0 : 2;
+		int myclass = (section == 1) ? 0 : 8;
 
 		//search for seat in section with type
-		for (int row = 0; row < 4; row++)
+		for (int i = myclass; i < 16; i++)
 		{
-			for (int col = myclass; col < 4; col++)
+			if (seats[i].avail == true)
 			{
-				if (seats[col][row].avail == true)
+				if (seats[i].getType() == type)
 				{
-					if (seats[col][row].getSection() == section)
-					{
-						seats[col][row].setOccupant(name);
-					}
+					seats[i].setOccupant(name);
+					return seats[i].name;
 				}
 			}
 		}
+		return null; //TODO throw exception
 	}
 	
 	//Check and return availability of sections to make sure requests can be met
-	//(returns array with: totalcount, windowcount, aislecount, firstclasscount, economycount)
-	public int[] checkAvailability()
+	//(returns array with: 0totalcount, 1windowcount, 2aislecount, 3firstclasscount, 4economycount)
+	public int[][] checkAvailability()
 	{
-		int total = 0;
-		int window = 0;
-		int aisle = 0;
-		int firstclass = 0;
-		int economy = 0;
-		
+
+		int fcwindow = 0; //first class window seat
+		int fcaisle = 0; //first class aisle seat
+		int ewindow = 0; //economy class window seat
+		int eaisle =0; //economy class aisle seat
+			
 		//for seat in list check if available. if/then to each category and total
-		for (int col = 0; col < 4; col++)
+		for (int i = 0; i < 16; i++)
 		{
-			for (int row = 0; row < 4; row++) 
-			{
-				if (seats[col][row].avail){
-					total += 1;
-					
-					if (col == 0 || col == 3) 
+			if (seats[i].avail){				
+				if (seats[i].getType() == "Window") 
+				{
+					if (seats[i].getSection() == 1)
 					{
-						window += 1;
+						fcwindow += 1;
 					}
-					else { 
-						aisle += 1;
-					}
-					
-					if (row < 2)
-					{
-						firstclass += 1;
-					}
-					else {
-						economy += 1;
-					}
-					
+					else { ewindow += 1;}
 				}
+				else { 
+					if (seats[i].getSection() == 1)
+					{
+						fcaisle += 1;
+					}
+					else { eaisle += 1;}
+				}					
 			}
 		}
-		int list[] = {total, window, aisle, firstclass, economy};
+		int list[][] = {{fcwindow, fcaisle},{ewindow, eaisle}};
 		return list;
 	}
 	
+	//private method to get seatID
+	private String getSeat(int num){
+		int mynumber = (num / 4) + 1;
+		char myletter = getLetter(num);
+		return "" + mynumber + myletter;
+	}
+	//private method to assign letter to seatID
 	
-	//private method to assign letter to seat
 	private char getLetter(int num)
 	{
-		switch (num) {
+		int cnum = num % 4;
+		switch (cnum) {
 			case 0:
 				return 'a';
 			case 1: 
@@ -114,20 +149,8 @@ public class Seats {
 			case 3:
 				return 'd';
 		}
-		//throw error
+		//TODO throw exception
 		return 'x';
-	}
-	//private method to assign section to seat
-	private char getSection(int num)
-	{
-		switch (num) {
-		case 0: case 1:
-			return 'a';
-		case 2: case 3:
-			return 'b';
-	}
-	//throw error
-	return 'x';
 	}
 
 }
