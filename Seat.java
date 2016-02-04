1aebe24074eb6a1d43c644b9d665e681bb5953e6
ryansutc <ryansutc@gmail.com>
2016-02-04 16:21:30 -0400

    Minor Updates and Refactoring
    Tweaked the Seat Class:
    - added a static method that implements the Java Comparator Class
      to allow sorting function on seats.

--- Seat.java
+++ Seat.java
@@ -1,10 +1,11 @@
 package airplaneseatbooker;
+import java.util.Comparator;
 
-public class Seat {
+public class Seat { //seat is a type parameter
 //Seats Class
 
-	public boolean avail = true; //made public for simplicity/speed
-	public String name = null;
+	protected boolean avail = true; //made directly accessible for simplicity/speed
+	protected String name = null;
 	private String occupant = null;
 	private int section; //1 = first class, 2 = economy
 	private String type; //Window or Aisle
@@ -15,6 +16,19 @@ public class Seat {
 		this.setSection(section);
 		this.setType(type);
 	}
+	//derived from site here: http://www.mkyong.com/java/java-object-sorting-example-comparable-and-comparator/
+	public static Comparator<Seat> SeatNameComparator 
+    				= new Comparator<Seat>() {
+
+		public int compare(Seat seat1, Seat seat2) {
+		
+			String seatName1 = seat1.getOccupant().toUpperCase();
+			String seatName2 = seat2.getOccupant().toUpperCase();
+			
+			//ascending order
+			return seatName1.compareTo(seatName2);
+		}
+	};
 	
 	public String getOccupant() 
 	{
@@ -43,3 +57,5 @@ public class Seat {
 		this.type = type;
 	}
 }
+
+	
